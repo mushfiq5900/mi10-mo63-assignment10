cf8a1234763346969de663b4cbcbb17c20cb5939
Mushfiq <mushfiquli@gmail.com>
2022-04-17 12:16:43 +0600

    Header Designed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
 const Header = () => {
     return (
-        <div className='container'>
-            <ul className="nav nav-pills justify-content-center">
-                <ul class="nav justify-content-center">
-                    <li className="nav-item">
-                        <Link to="/">Home</Link>
-                        <Link to="/">Services</Link>
-                        <Link to="/">Contact</Link>
-                        <Link to="/">About</Link>
-                    </li>
-                </ul>
-            </ul>
+        <div className="container-fluid bg-light">
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            src="favicon.png"
+                            width="50"
+                            height="50"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Brand href="#home">Splash Photography</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Link className='nav-link' to="/">Home</Link>
+                            <Link className='nav-link' to="/services">Services</Link>
+                            <Link className='nav-link' to="/blog">Blog</Link>
+                            <Link className='nav-link' to="/about">About</Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
